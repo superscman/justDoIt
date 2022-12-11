ab02b1e976bfbbc9f28656c87a58f5ac203b2044
superscman <vdcf2000@gmail.com>
2022-12-11 21:17:13 +0900

    Initial commit

---
+++ justDoIt/src/main/java/com/codestates/justDoIt/JustDoItApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.justDoIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustDoItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustDoItApplication.class, args);
+	}
+
+}
---
+++ justDoIt/src/test/java/com/codestates/justDoIt/JustDoItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.justDoIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustDoItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
